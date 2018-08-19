83bf40bba0546067d69ae55c21afa87a35699def
Igor <cableclark@gmail.com>
2018-08-19 15:10:21 +0200

    fisrt commmit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$quotes = [
+    [   
+        "author"=> "Dr. Seuss",
+        "quote"=> "Don't cry because it's over, smile because it happened."
+    ],
+    [   
+        "author"=> "Marilyn Monroe",
+        "quote"=> "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."
+    ],
+ 
+    [   
+        "author"=> "Oscar Wilde",
+        "quote"=> "Be yourself; everyone else is already taken."
+    ],
+ 
+    [   
+        "author"=> "Albert Einstein",
+        "quote"=> "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+    ],
+ 
+    [   
+        "author"=> "Robert Frost",
+        "quote"=> "You only live once, but if you do it right, once is enough.."
+    ],
+ 
+    [   
+        "author"=> " Friedrich Nietzsche",
+        "quote"=> "Don’t walk in front of me… I may not follow
+        Don’t walk behind me… I may not lead
+        Walk beside me… just be my friend."
+    ]
+
+] ;
+
+$quote= $quotes[rand(0, count($quotes)-1)];
+
+echo $quote["quote"]. " <br> <b>" . 
+$quote["author"] . "<br>" ;
+
+
+?>
